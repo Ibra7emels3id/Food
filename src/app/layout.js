934e515689eee8aabd9a50a76c08675a29f2a897
Ibrahem Elsaid <158269891+Ibra7emels3id@users.noreply.github.com>
2024-08-29 23:18:28 +0300

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Provider } from "react-redux";
 import { Store } from "../lib/store";
-import AuthProvider from "../providers/AuthProvider";
+import AuthProvider from "../Providers/AuthProvider.js";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,15 +15,15 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={inter.className}>
-                <Provider store={Store}>
-                    <AuthProvider>
+                <AuthProvider>
+                    <Provider store={Store}>
                         <Header />
                         <main>
                             {children}
                         </main>
                         <Footer />
-                    </AuthProvider>
-                </Provider>
+                    </Provider>
+                </AuthProvider>
             </body>
         </html>
     );
